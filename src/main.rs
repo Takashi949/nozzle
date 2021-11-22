2ea7b28d2d3ec5f8a3916c2035781f74a951961e
Takashi949 <byouyamada@gmail.com>
2021-11-23 02:08:50 +0900

    pdp0 pjp0まで

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
+struct Nozzle{
+    Dc : f64,
+    De : f64,
+    Le : f64,
+    Ae : f64,
+    Ac : f64,
+}
+
+impl Nozzle{
+    fn calcAx(&self, x: f64) -> f64{
+        let Dx = (self.De - self.Dc) * x / self.Le + self.Dc;
+        return std::f64::consts::PI / 4.0 * Dx.powf(2.0); 
+    }
+
+    fn new(Dcm:f64, Dem:f64, Lem:f64) -> Nozzle {
+        Nozzle {Dc : Dcm, De: Dem, Le: Lem, Ae: 0.0, Ac: 0.0}
+    }
+
+    fn init(&mut self){
+        self.Ae = std::f64::consts::PI / 4.0 * self.De.powf(2.0); 
+        self.Ac = std::f64::consts::PI / 4.0 * self.Dc.powf(2.0); 
+        println!("Ae = {:.2}[mm2]   A* = {:.2}[mm2]", self.Ae*1.0e6, self.Ac*1.0e6);
+    }
+}
+
 fn calcPc(P0: &f64, gammma: &f64) -> f64 {
-    let pc = P0*( 2.0 / (gammma + 1.0) ).powf(( gammma / (gammma - 1.0) ));
+    let pc = P0*( 2.0 / (gammma + 1.0) ).powf(gammma / (gammma - 1.0));
     println!("p* = {:.2}[kPa]", pc/1000.0);
     return pc;
 }
@@ -8,11 +33,31 @@ fn calcTc(T0: &f64, gammma : &f64 ) -> f64{
     println!("T* = {:.2}[K]", Tc);
     return Tc;
 }
-
+fn calc_pd_by_p0(kappa: &f64, AeAc: &f64) -> f64{
+    let mut pdp0:f64 = 0.528;
+    while AeAc - ( (kappa - 1.0) / 2.0 * ( 2.0 / ( kappa + 1.0 ) ).powf( ( kappa + 1.0 )/( kappa - 1.0) )
+            / ( pdp0.powf(2.0 / kappa ) - pdp0.powf( ( kappa + 1.0 )/kappa ) )  ).powf(0.5)
+            > 1.0e-6
+    {
+        pdp0 += 0.01;
+    }
+    println!("pd/p0 = {:.3}", pdp0);
+    return pdp0;
+}
+fn calc_pj_by_p0(kappa: &f64, AeAc: &f64) -> f64{
+    let mut pjp0:f64 = 0.528;
+    while AeAc - ( (kappa - 1.0) / 2.0 * ( 2.0 / ( kappa + 1.0 ) ).powf( ( kappa + 1.0 )/( kappa - 1.0) )
+            / ( pjp0.powf(2.0 / kappa ) - pjp0.powf( ( kappa + 1.0 )/kappa ) )  ).powf(0.5)
+             > 1.0e-6
+    {
+        pjp0 -= 0.01;
+    }
+    println!("pj/p0 = {:.3}", pjp0);
+    return pjp0;
+}
 fn main() {
-    let mut inpt = String::new();
-    std::io::stdin().read_line(&mut inpt);
-    let nozzleType = (inpt.starts_with("y"));//true => con-di nozzle
+    let mut nozzle = Nozzle::new(1.0e-2, 1.5e-2, 2.0e-2);
+    nozzle.init();
  
     let kappa : f64 = 1.40;
     let Mw = 28.8;
@@ -29,17 +74,21 @@ fn main() {
     let Rowc = P0/(R*T0);
 
     let mut pe = 0.0;
-    if (Pa > pc) {
+    if (pc < Pa && Pa < P0) {
         pe = Pa;
-        println!("亜音速噴流");
+        println!("ノズル全域で亜音速流れ");
     }
     else if (Pa == pc){
         pe = pc;
-        println!("音速噴流");
+        println!("スロートで音速それ以外で亜音速");
     }
     else if (Pa < pc){
         pe = pc;
         println!("不足膨張噴流");
+        //let Pe = pe * ( 1.0 + ( kappa - 1.0 ) / 2.0 *M^2).powf( kappa / ( kappa - 1.0 ));
+        let AeAc = &nozzle.Ae / &nozzle.Ac;
+        let pdp0 = calc_pd_by_p0(&kappa, &AeAc); 
+        let pjp0 = calc_pj_by_p0(&kappa, &AeAc);       
     }
     println!("pe = {:.2}[kPa]", pe/1000.0);
     let ue = ( 2.0 * kappa / ( kappa - 1.0) * R * T0 * ( 1.0 - ( pe / P0 ).powf( ( kappa - 1.0 ) / kappa ) ) ).powf(0.5);
