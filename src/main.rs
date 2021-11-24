127a2a83ed4f446aa870bd2b59461eacb39937e4
Takashi949 <byouyamada@gmail.com>
2021-11-24 18:29:40 +0900

    関数化

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
-use std::io::Write;
+use std::{io::Write, num::NonZeroI128, ops::Add};
 
 const ERROR :f64 = 1.0e-3;
 const delta :f64 = 1.0e-4;
+
+enum NAGARE {
+    MthL1,
+    MthG1MeL1,
+    suehiro,
+    deguti,
+    ka,
+    saiteki,
+    husoku,
+    humei,
+}
 
 #[derive(Default)]
 struct Nozzle{
@@ -171,91 +182,84 @@ fn calc_M (kappa : &f64, pp0: &f64) -> f64 {
     return M;
 } 
 
-fn main() -> Result<(), Box<std::error::Error>>{
-    let mut nozzle = Nozzle::new(20.0e-3, 10.0e-3, 10.0e-3, 17.320508e-3, 40.0e-3);
-    nozzle.init();
+fn check_nagare_keitai(nozzle : &Nozzle, kappa : &f64, PbP0: f64) -> NAGARE{
     let AeAth = &nozzle.Ae / &nozzle.Ath;
-    //let AeAc = 2.403;
-    println!("AeAth = {:.3}",AeAth);
-
-    let kappa : f64 = 1.40;
-    let Mw = 28.8;
-    let RR = 8.3143e3;//J/kmolK
-    let R = RR/Mw;
-    println!("R = {:.3}[kJ/kgK]", R/1000.0);
-
-    let P0 : f64 = 105.0e3;//全域亜音速
-    //let P0 = 150.0e3;//衝撃波
-    let T0 : f64 = 290.0;// + 273.15;//K
-    let Pa = 101.325e3;//Pa
-
-    let pc = calcPc(&P0, &kappa);
-    let Tc = calcTc(&T0, &kappa);
-
-    let PbP0 = Pa / P0;
-    println!("PbP0 = {:.3}", PbP0);
-    let mut pep0 = 0.0;
-    let mut pe = 0.0;
-
     let pdp0 = calc_pd_by_p0(&kappa, &AeAth); 
     let pjp0 = calc_pj_by_p0(&kappa, &AeAth); 
-    let php0 = calc_ph_by_p0(&kappa, &AeAth);      
-
-    let mut Me = 0.0;
-    let mut Mth = 0.0;
-
-    let mut isIsentropic = true;
-
-    //衝撃波がある場合
-    let mut AmAc = 0.0; 
+    let php0 = calc_ph_by_p0(&kappa, &AeAth);   
 
     if pdp0 <= PbP0 && PbP0 < 1.0 {
         //亜音速流
-        pep0 = PbP0;
         if (pdp0 < PbP0 && PbP0 < 1.0){
             println!("全域で亜音速");
-            Mth = calc_M(&kappa, &PbP0);
+            return NAGARE::MthL1;
         }
         else {
             println!("スロートで音速、その他で亜音速");
-            Mth = 1.0;
+            return NAGARE::MthG1MeL1;
         }
-        Me = calc_sub_sonic_M2_from_M1_by_AA(&kappa, &Mth, &AeAth);
     }
     else{ 
         //スロートで音速
-        Mth = 1.0;
-        Me = calc_super_sonic_M2_from_M1_by_AA(&kappa, &Mth, &AeAth);//衝撃波がある場合は上書きされる
         if php0 < PbP0 && PbP0 < pdp0 {
             println!("非等エントロピー");
             println!("ノズルの末広部に垂直衝撃波");
-            isIsentropic = false;
+            return NAGARE::suehiro;
+        }
+        else if php0 == PbP0 { 
+            println!("出口に垂直衝撃波");
+            return NAGARE::deguti;
+        }
+        else if pjp0 < PbP0 && PbP0 < php0 {
+            println!("過膨張噴流");
+            return NAGARE::ka;
+        }
+        else if pjp0 == PbP0 {
+            println!("適正膨張");
+            return NAGARE::saiteki;
+        } 
+        else if PbP0 < pjp0 {
+            println!("不足膨張");
+            return NAGARE::husoku;       
+        }
+    }
+    return NAGARE::humei;
+}
 
+fn calc_Mx(nozzle : &Nozzle, kappa : &f64, PbP0: f64, nagare: NAGARE) -> Vec<quantity>{
+    let AeAth = &nozzle.Ae / &nozzle.Ath;
+    let mut Mth = 1.0;//MthL1のときだけ変更
+    let mut Me = 0.0;
+
+    //衝撃波がある場合
+    let mut AmAc= 0.0;
+
+    match nagare {
+        NAGARE::MthL1 => {
+            Mth = calc_M(&kappa, &PbP0);
+            Me = calc_sub_sonic_M2_from_M1_by_AA(&kappa, &Mth, &AeAth);
+        },
+        NAGARE::MthG1MeL1 => {
+            Me = calc_sub_sonic_M2_from_M1_by_AA(&kappa, &Mth, &AeAth);
+        },
+        NAGARE::suehiro => {
             let M1 = calc_M_before_shock(&kappa, &PbP0);
             let M2 = calc_M_after_shock(&kappa, &M1);
             AmAc = calc_AAc(&kappa, &M1);//衝撃波発生位置の断面積比
             let xM = nozzle.calc_xD(&AmAc);//衝撃波の発生位置
             let AeAm = AeAth / AmAc;
             Me = calc_sub_sonic_M2_from_M1_by_AA(&kappa, &M2, &AeAm);
-        }
-        else if php0 == PbP0 { 
-            println!("出口に垂直衝撃波");
-        }
-        else if pjp0 < PbP0 && PbP0 < php0 {
-            println!("過膨張噴流");
-        }
-        else if pjp0 == PbP0 {
-            println!("適正膨張");
-        } 
-        else if PbP0 < pjp0 {
-            println!("不足膨張");
+        },
+        _=>{
+            Me = calc_super_sonic_M2_from_M1_by_AA(&kappa, &Mth, &AeAth);//衝撃波がある場合は上書きされる
         }
     }
-    println!("Mth = {:.3}", Mth);
+    println!("Me = {:.3}    Mth = {:.3}",Me, Mth);
 
     //ここから格子計算
     const N : usize =  256 * 2;
-    let mut Quans : [quantity; N] = [quantity{x:0.0, Mx:0.0, ..Default::default()}; N];
+    //let mut Quans : [quantity; N] = [quantity{x:0.0, Mx:0.0, ..Default::default()}; N];
+    let mut Quans : Vec<quantity> = Vec::new();
     let dx = &nozzle.xe / N as f64;
 
     for i in 1..N {
@@ -274,15 +278,7 @@ fn main() -> Result<(), Box<std::error::Error>>{
             let AxAth = nozzle.calc_AxD(&x) / nozzle.Ath;
             rx = nozzle.calc_DxD(&x) / 2.0;
 
-            if Me < 1.0 && isIsentropic{
-                //等エントロピー、出口で亜音速
-                Mx = calc_sub_sonic_M2_from_M1_by_AA(&kappa, &Mth, &AxAth);
-            }
-            else if isIsentropic {
-                //等エントロピー、出口で超音速
-                Mx = calc_super_sonic_M2_from_M1_by_AA(&kappa, &Mth, &AxAth);
-            }
-            else {
+            if let NAGARE::suehiro = nagare{
                 //非等エントロピー
                 if AxAth < AmAc {
                     //衝撃波の上流 
@@ -294,6 +290,14 @@ fn main() -> Result<(), Box<std::error::Error>>{
                     Mx = calc_sub_sonic_M2_from_M1_by_AA(&kappa, &Mth, &AxAm);            
                 }
             }
+            else if Me < 1.0{
+                //等エントロピー、亜音速
+                Mx = calc_sub_sonic_M2_from_M1_by_AA(&kappa, &Mth, &AxAth);
+            }
+            else {
+                //等エントロピー、超音速
+                Mx = calc_super_sonic_M2_from_M1_by_AA(&kappa, &Mth, &AxAth);
+            }
         }
 
         //Mac_Array[i] = kyokushoM;
@@ -301,14 +305,41 @@ fn main() -> Result<(), Box<std::error::Error>>{
         //println!("{}",i);
 
         let res = quantity {x, Mx, rx};
-        Quans[i] = res;
+        Quans.push(res);
     }
+    return Quans;
+}
+fn main() -> Result<(), Box<std::error::Error>>{
+    let mut nozzle = Nozzle::new(20.0e-3, 10.0e-3, 10.0e-3, 17.320508e-3, 40.0e-3);
+    nozzle.init();
+    let AeAth = &nozzle.Ae / &nozzle.Ath;
+    //let AeAc = 2.403;
+    println!("AeAth = {:.3}",AeAth);
+
+    let kappa : f64 = 1.40;
+    let Mw = 28.8;
+    let RR = 8.3143e3;//J/kmolK
+    let R = RR/Mw;
+    println!("R = {:.3}[kJ/kgK]", R/1000.0);
+
+    //let P0 : f64 = 102.0e3;//全域亜音速
+    let P0 = 150.0e3;//衝撃波
+    let T0 : f64 = 290.0;// + 273.15;//K
+    let Pa = 101.325e3;//Pa
+
+    let PbP0 = Pa / P0;
+    println!("PbP0 = {:.3}", PbP0);
+
+    let mut Me = 0.0;
+    let mut Mth = 0.0;
+
+    let nagare = check_nagare_keitai(&nozzle, &kappa, PbP0);
+    let mut quans = calc_Mx(&nozzle, &kappa, PbP0, nagare);
 
     //結果の出力
-    println!("Me = {:.3}", Me);
-    let mut f = std::fs::File::create(format!("./{:.3}_{:.3}_{:.3}.csv", Mth, Me, N))?;
-    for i in 0 .. N {
-       writeln!(f, "{},{}, {}", Quans[i].x, Quans[i].Mx, Quans[i].rx)?;
+    let mut f = std::fs::File::create(format!("./{}.csv", PbP0))?;
+    for q in quans {
+       writeln!(f, "{},{}, {}", q.x, q.Mx, q.rx)?;
     }
     f.flush()?;
     Ok(())
